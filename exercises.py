c796a0a6ded9b0299277735eeddae92437986077
GuillN <gnaassan@gmail.com>
2020-03-12 15:05:02 +0100

    test done

--- exercises.py
+++ exercises.py
@@ -1,43 +1,38 @@
+import collections
+
 def is_anagram(s1, s2):
-    """
-    Write an algorithm that returns whether s1 and s2 are anagrams of each other, i.e.
-    if s1 and s2 contain the same letters in a possibly different order.
-    E.g.: "abc" and "cab" are anagrams, "aab" and "bba" are not.
-    :param s1: string
-    :param s2: string
-    :return: True or False
-    """
-
-    # Write your code here
-    pass
-
+    return sorted(s1) == sorted(s2)
 
 def check_parenthesis_consistency(string):
-    """
-    Write an algorithm that determines if the parenthesis (round brackets "()") in a string are properly balanced.
-    An expression is said to be properly parenthesised if it has the form "(p)" or "pq", where p and q are
-    properly parenthesised expressions. Any string (including an empty string) that does not contain any parenthesis
-    is properly parenthesised.
-    E.g.: "()()" is properly parenthesised, "(()" is not.
-    :param string: the string to analyse.
-    :return: True if the parentheses are balanced, False if not.
-    """
-
-    # Write your code here
-    pass
-
+    count = 0
+    for char in string:
+        if char == '(':
+            count += 1
+        if char == ')':
+            count -= 1
+    return count == 0
 
 def shortest_path(start, end, maze):
-    """
-    Write an algorithm that finds the shortest path in a maze from start to end
-    The maze is represented by a list of lists containing 0s and 1s:
-    0s are walls, paths cannot go through them
-    The only movements allowed are UP/DOWN/LEFT/RIGHT
-    :param start: tuple (x_start, y_start) - the starting point
-    :param end: tuple (x_end, y_end) - the ending point
-    :param maze: list of lists - the maze
-    :return: list of positions [(x1, y1), (x2, y2), ...] representing the shortest path in the maze
-    """
-
-    # Write your code here
-    pass
+    # init queue and visited arrays
+    queue = collections.deque([[start]])
+    visited = {start}
+    # init width and height of the maze
+    width = len(maze[0])
+    height = len(maze)
+    # start operations
+    while queue:
+        # add queue element to path
+        path = queue.popleft()
+        x, y = path[-1]
+        # check end condition: if ended return path
+        if (x, y) == end:
+            return path
+        # if not ended continue building path by checking neighbors
+        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
+            # check if we are within maze boundaries, if we hit a wall and if we are backtracking
+            if 0 <= y2 < width and 0 <= x2 < height and maze[x2][y2] != 0 and (x2, y2) not in visited:
+                # add neighboring elements to queue and visited arrays
+                queue.append(path + [(x2, y2)])
+                visited.add((x2, y2))
+    # if there is no path from start to end
+    return False
